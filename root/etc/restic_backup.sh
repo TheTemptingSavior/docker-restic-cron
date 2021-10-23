37a44b8cc3ac7749b5dd07112eae1ac58b3fc10b
Ethan Cotterell <ethancotterell@gmail.com>
2021-10-23 16:14:55 +0100

    restic: removed uneeded scipts

--- root/etc/restic_backup.sh
+++ root/etc/restic_backup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Runs an incremental backup of all the data within the 
+# `/data` directory to the remote repository
+
 set -e
 
 function do_print() {
@@ -9,6 +12,7 @@ function do_print() {
 
 do_print "Starting new restic backup"
 
+
 do_print "Running pre-backup scripts"
 # Run pre-backup scripts
 for f in /config/scripts/before/*; do
@@ -17,18 +21,33 @@ for f in /config/scripts/before/*; do
     fi
 done
 
-do_print "Running restic backup of $RESTIC_REPOSITORY"
 
-# check for a hostname in env, if not present use container hostname
-[[ -z "$RESTIC_HOSTNAME" ]] && $RESTIC_HOSTNAME=$(hostname)
+# check for a hostname in env, if not present exit because the hostname is
+# tied to the repository name
+[[ -z "$RESTIC_HOSTNAME" ]] && exit 1
 
+
+do_print "Running restic backup of $RESTIC_REPOSITORY/$RESTIC_HOSTNAME"
 restic \
-    --repo $RESTIC_REPOSITORY \
+    --repo $RESTIC_REPOSITORY/$RESTIC_HOSTNAME \
     $RESTIC_OPTIONAL_ARGS \
     backup \
     --host $RESTIC_HOSTNAME \
     $RESTIC_OPTIONAL_BACKUP_ARGS \
     /data
+
+
+do_print "Running restic backup of $RESTIC_REPOSITORY/$RESTIC_HOSTNAME"
+restic \
+    --repo $RESTIC_REPOSITORY/$RESTIC_HOSTNAME \
+    $RESTIC_OPTIONAL_ARGS \
+    forget \
+    --host=$RESTIC_HOSTNAME \
+    --keep-daily 7 \
+    --keep-weekly 4 \
+    --keep-monthly 6 \
+    --keep-yearly 2 \
+
 
 do_print "Running post-backup scripts"
 # Run post-backup scripts
@@ -37,3 +56,6 @@ for f in /config/scripts/after/*; do
         bash $f
     fi
 done
+
+
+do_print "Completed restic backup of $RESTIC_REPOSITORY/$RESTIC_HOSTNAME"
